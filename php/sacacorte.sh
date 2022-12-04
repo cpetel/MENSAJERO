2970c8e2f0117fbaa1a88695329b8c44455ad6c3
Nico <cpetel.conmutacion@gmail.com>
2022-12-04 13:35:27 -0300

    from0

--- php/sacacorte.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#monta
-sudo mount -t cifs //192.168.0.11/CortesEnergia -o username=elastix@cpe.com,password=s1st3*cpe /mnt/ntserver
-#rm /mnt/ntserver/estado.txt
-rm /mnt/ntserver/mensaje.txt
-rm /mnt/ntserver/mapa.svg
-#desmonta
-sudo umount /mnt/ntserver	
--- php/sacacorteprueba.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#monta
-sudo mount -t cifs //192.168.0.11/CortesEnergia -o username=elastix@cpe.com,password=s1st3*cpe /mnt/ntserver
-#rm /mnt/ntserver/estado.txt
-rm /mnt/ntserver/pruebas/mensaje.txt
-rm /mnt/ntserver/pruebas/mapa.svg
-#desmonta
-sudo umount /mnt/ntserver	
